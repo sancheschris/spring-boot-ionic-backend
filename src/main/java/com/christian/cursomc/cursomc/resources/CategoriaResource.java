60bedd14f2c008badc3fb1b9fb6072bb4ca5da67
Christian Santos <sancheschris07@gmail.com>
2020-03-16 14:05:56 -0300

    Testando primeiro dominio

--- src/main/java/com/christian/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/christian/cursomc/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.christian.cursomc.cursomc.resources;
 
+import com.christian.cursomc.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar() {
-        return "REST está funcionando!";
+    public List<Categoria> listar() {
+
+        Categoria cat = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat);
+        lista.add(cat2);
+
+        return lista;
     }
 }
