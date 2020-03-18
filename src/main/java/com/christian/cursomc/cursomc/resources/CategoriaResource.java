9aeb4f60290dfe891beebaf7fbc13a6afcc42158
Christian Santos <sancheschris07@gmail.com>
2020-03-18 12:28:20 -0300

    Inserindo uma nova categoria com POST

--- src/main/java/com/christian/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/christian/cursomc/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,10 @@ import com.christian.cursomc.cursomc.services.CategoriaService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,11 +21,17 @@ public class CategoriaResource {
     private CategoriaService categoriaService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<?> listar(@PathVariable Integer id) {
+    public ResponseEntity<?> find(@PathVariable Integer id) {
 
         Categoria obj = categoriaService.buscar(id);
         return ResponseEntity.ok().body(obj);
     }
 
-
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = categoriaService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 }
