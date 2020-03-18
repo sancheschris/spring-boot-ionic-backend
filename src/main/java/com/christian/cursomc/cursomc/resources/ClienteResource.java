e10e0db061d8d3a91de1245246390dfd7d6ad355
Christian Santos <sancheschris07@gmail.com>
2020-03-18 17:36:15 -0300

    PUT,DELETE e GET para Cliente

--- src/main/java/com/christian/cursomc/cursomc/resources/ClienteResource.java
+++ src/main/java/com/christian/cursomc/cursomc/resources/ClienteResource.java
@@ -2,14 +2,18 @@ package com.christian.cursomc.cursomc.resources;
 
 import com.christian.cursomc.cursomc.domain.Categoria;
 import com.christian.cursomc.cursomc.domain.Cliente;
+import com.christian.cursomc.cursomc.dto.CategoriaDTO;
+import com.christian.cursomc.cursomc.dto.ClienteDTO;
 import com.christian.cursomc.cursomc.services.CategoriaService;
 import com.christian.cursomc.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -25,5 +29,38 @@ public class ClienteResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+        Cliente obj = clienteService.fromDTO(objDto);
+        obj.setId(id);
+        obj = clienteService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll() {
+        List<Cliente> list = clienteService.findAll();
+        List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<ClienteDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linePerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+        Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+
+        Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+        return ResponseEntity.ok().body(listDTO);
+    }
 
 }
--- src/main/java/com/christian/cursomc/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/christian/cursomc/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import com.christian.cursomc.cursomc.services.exceptions.ObjectNotFoundException
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,6 +26,17 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getLocalizedMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+        for (FieldError x : e.getBindingResult().getFieldErrors()) {
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
