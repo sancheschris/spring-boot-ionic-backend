002de1826722a44f27a1f38d2775ffca48740637
Christian Santos <sancheschris07@gmail.com>
2020-03-18 10:57:51 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/christian/cursomc/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.christian.cursomc.cursomc.repositories;
+
+import com.christian.cursomc.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+
+}
---
+++ src/main/java/com/christian/cursomc/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.christian.cursomc.cursomc.repositories;
+
+import com.christian.cursomc.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
